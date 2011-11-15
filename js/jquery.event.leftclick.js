7e9ae923b2c4a3109c28c93326859a7cdb75920a
fliptheweb <itisshortemailaddress@gmail.com>
2011-11-15 16:01:56 +1100

    Base special event from Brandon Aaron article

--- js/jquery.event.leftclick.js
+++ js/jquery.event.leftclick.js
@@ -1,23 +1,28 @@
+/**
+ * @author fliptheweb
+ * Based on http://brandonaaron.net/blog/2010/02/25/special-events-the-changes-in-1-4-2
+ */
 (function($) {
 	$.event.special.leftclick = {
-		//bind leftclick event
-		setup: function(eventData, namespaces) {
-			var $this = $(this);
+		add: function( handler, data, namespaces ) {
 
-			$(this).bind("click.leftclick", function(){
-
-			});
-
-			return ;
 		},
-		//unbind leftclick event
-		teardown: function(namespaces) {
-			var $this = $(this);
 
+		setup: function( data, namespaces ) {
+			$(this).bind("click.leftclick", function(){
+				console.log("click to div");
+			});
+		},
+
+		remove: function( namespaces ) {
+		},
+
+		teardown: function( namespaces ) {
 			$this.unbind("click.leftclick");
-			
-			return;
+		},
+
+		handler: function( event ) {
+
 		}
 	};
-
 })(jQuery);
